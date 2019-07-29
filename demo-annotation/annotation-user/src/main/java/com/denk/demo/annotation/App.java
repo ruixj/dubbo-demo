fccf1cf1611201a0f8b286bf13c89ac417de9098
ruixj <ustb_ruixj@aliyun.com>
2019-07-29 21:51:09 +0800

    test

--- demo-annotation/annotation-user/src/main/java/com/denk/demo/annotation/App.java
+++ demo-annotation/annotation-user/src/main/java/com/denk/demo/annotation/App.java
@@ -1,6 +1,8 @@
 package com.denk.demo.annotation;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
@@ -10,15 +12,19 @@ import org.springframework.context.annotation.Bean;
  * Hello world!
  */
 @SpringBootApplication
-public class App {
-    public static void main(String[] args) {
+public class App     {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(App.class);
+        System.out.println("Provider started.");
+        System.in.read(); // press any key to exit
     }
 
-    @Bean
-    public EmbeddedServletContainerFactory embeddedServletContainerFactory() {
-        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
-        factory.setPort(9090);
-        return factory;
-    }
+
+
+//    @Bean
+//    public EmbeddedServletContainerFactory embeddedServletContainerFactory() {
+//        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+//        factory.setPort(9090);
+//        return factory;
+//    }
 }
